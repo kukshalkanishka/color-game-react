14653957566008dcf2e71677b06762cf9ad554fe
Kanishka kuksal <37463477+kukshalkanishka@users.noreply.github.com>
2019-04-13 12:37:49 +0530

    Updates

---
+++ precache-manifest.bf6ca9cb350aea235dbe548929671f0d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e9ea3bf7629b5a53afc",
+    "url": "/static/css/main.dd3e9e17.chunk.css"
+  },
+  {
+    "revision": "5e9ea3bf7629b5a53afc",
+    "url": "/static/js/main.93c26c28.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "977d56a9d9faa5f0809b",
+    "url": "/static/js/2.46048444.chunk.js"
+  },
+  {
+    "revision": "0c8607a7d581962d577b8e0de09516ae",
+    "url": "/index.html"
+  }
+];
