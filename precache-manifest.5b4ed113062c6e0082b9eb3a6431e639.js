2e7325401a72dbd02c58a38cb179131fa9e113c2
Kanishka kuksal <37463477+kukshalkanishka@users.noreply.github.com>
2019-04-16 17:21:45 +0530

    Updates

---
+++ precache-manifest.5b4ed113062c6e0082b9eb3a6431e639.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9172a0d0cc43c60a58f",
+    "url": "/color-game-react/static/css/main.dd3e9e17.chunk.css"
+  },
+  {
+    "revision": "f9172a0d0cc43c60a58f",
+    "url": "/color-game-react/static/js/main.10541982.chunk.js"
+  },
+  {
+    "revision": "7b1682096e7079165e90",
+    "url": "/color-game-react/static/js/runtime~main.8657b969.js"
+  },
+  {
+    "revision": "977d56a9d9faa5f0809b",
+    "url": "/color-game-react/static/js/2.46048444.chunk.js"
+  },
+  {
+    "revision": "ef5f3ce963eb1122dde150f9cba90274",
+    "url": "/color-game-react/index.html"
+  }
+];
